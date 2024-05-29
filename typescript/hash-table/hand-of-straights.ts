5b5677553c5e2ce95f27803fa93e49025863e055
Son Tran <son.tran@enouvo.com>
2024-05-29 14:19:07 +0700

    feat(master): update longest substring

---
+++ typescript/hash-table/hand-of-straights.ts
@@ -0,0 +1,7 @@
+// Source: https://leetcode.com/problems/hand-of-straights/description/
+
+function isNStraightHand(hand: number[], groupSize: number): boolean {
+  if (hand.length % groupSize !== 0) {
+    return false;
+  }
+}
